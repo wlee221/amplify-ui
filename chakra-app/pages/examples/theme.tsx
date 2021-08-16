f69ebe581a7792f2cf6e31758542fb45925e985a
Chenwei Zhang <chenwz@amazon.com>
2021-08-15 17:59:20 -0700

    chore(wip): theming examples

---
+++ chakra-app/pages/examples/theme.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from '@aws-amplify/ui-react';
+import { Box } from '@chakra-ui/react';
+
+const App = () => {
+  return (
+    <View>
+      <Box bg="orange.100">Chakra Theming</Box>
+    </View>
+  );
+};
+
+export default App;
