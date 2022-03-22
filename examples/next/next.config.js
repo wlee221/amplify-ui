c62f976509af80365a5911af73131bbf5865f307
Scott Rees <6165315+reesscot@users.noreply.github.com>
2022-03-22 12:52:51 -0700

    add csp config for next js

--- examples/next/next.config.js
+++ examples/next/next.config.js
@@ -1,6 +1,30 @@
 const { withGlobalCss } = require('next-global-css');
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' 'nonce-fakeNonce';
+  child-src example.com;
+  style-src 'self' example.com rsms.me fonts.googleapis.com fonts.gstatic.com;
+  font-src 'self' rsms.me;
+  img-src 'self' images.unsplash.com  
+`;
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+];
+
 module.exports = withGlobalCss()({
   reactStrictMode: true,
   pageExtensions: ['page.tsx'],
+  // async headers() {
+  //   return [
+  //     {
+  //       // Apply these headers to all routes in your application.
+  //       source: '/:path*',
+  //       headers: securityHeaders,
+  //     },
+  //   ];
+  // },
 });
