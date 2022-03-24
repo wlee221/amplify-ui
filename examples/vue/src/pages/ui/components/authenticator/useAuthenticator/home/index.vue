8e02effe015a785688fb167f48179ddffc02a743
wlee221 <willeea@amazon.com>
2022-03-24 04:03:57 -0700

    Add hidden authenticator

--- examples/vue/src/pages/ui/components/authenticator/useAuthenticator/home/index.vue
+++ examples/vue/src/pages/ui/components/authenticator/useAuthenticator/home/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { toRefs } from 'vue';
 import Amplify from 'aws-amplify';
-import { useAuthenticator } from '@aws-amplify/ui-vue';
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-vue';
 import '@aws-amplify/ui-vue/styles.css';
 import aws_exports from '../aws-exports';
 
@@ -11,5 +11,7 @@ const { user } = toRefs(useAuthenticator());
 </script>
 
 <template>
+  <!-- TODO: this authenticator shouldn't be needed -->
+  <authenticator></authenticator>
   <div>Hello, {{ user?.username }}!</div>
 </template>
