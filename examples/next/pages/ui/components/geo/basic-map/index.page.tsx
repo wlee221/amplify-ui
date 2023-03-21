28276b60b5aa596b9d133c9a336a131bdb183c74
Sreeram Sama <12038441+sreeramsama@users.noreply.github.com>
2023-03-21 15:52:18 -0700

    chore: test codeql failures

--- examples/next/pages/ui/components/geo/basic-map/index.page.tsx
+++ examples/next/pages/ui/components/geo/basic-map/index.page.tsx
@@ -1,4 +1,4 @@
-import { MapView } from '@aws-amplify/ui-react';
+import { MapView, LocationSearch } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 
 import '@aws-amplify/ui-react/styles.css';
@@ -8,5 +8,6 @@ import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
 export default function BasicMap() {
+  const unusedVar = '';
   return <MapView />;
 }
